c1900dd112930351b758e1dec2efd5d70282a3e4
unknown <amincxo@gmail.com>
2024-09-12 23:15:13 +0330

    interceptor request

--- src/App.jsx
+++ src/App.jsx
@@ -10,19 +10,19 @@ function App() {
     const [todos , setTodos] = useState([]);
 
     useEffect(( )=>{
-        // axios.get("https://jsonplaceholder.typicode.com/todos")
-        // .then(res => setTodos(res.data));
+        axios.get("https://jsonplaceholder.typicode.com/todos")
+        .then(res => console.log(res.data));
 
-        axios.all([
-            axios.get("https://jsonplaceholder.typicode.com/todos"),
-            axios.get("https://jsonplaceholder.typicode.com/posts"),
-            axios.get("https://jsonplaceholder.typicode.com/users"),
-        ])
-        .then(axios.spread((todos , posts , users)=> {
-            console.log(todos)
-            console.log(posts)
-            console.log(users)
-        }))
+        // axios.all([
+        //     axios.get("https://jsonplaceholder.typicode.com/todos"),
+        //     axios.get("https://jsonplaceholder.typicode.com/posts"),
+        //     axios.get("https://jsonplaceholder.typicode.com/users"),
+        // ])
+        // .then(axios.spread((todos , posts , users)=> {
+        //     console.log(todos)
+        //     console.log(posts)
+        //     console.log(users)
+        // }))
     },[])
 
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import axios from 'axios'
+
+axios.interceptors.request.use(
+    (request) => {
+        request.headers.Authorization ="token";
+        console.log(`${request.method} requst sent to ${request.url}`);
+        return request;
+    }
+    ,(error) => {
+        console.log(error);
+        return Promise.reject(error);
+    }
+)
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
